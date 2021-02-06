08f5efab5f7b21cace1eefe8d23e40f0e97b4670
michael lee <mleeteak13@gmail.com>
2021-02-06 17:00:16 -0500

    Added more materials

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ import ARoom from './ARoom'
 import Model from './Wash'
 import Pump from './Npump'
 import Store from './Ncstore2'
-import './App.css';
+import './App.css'
+import { softShadows, Sky } from '@react-three/drei'
+
+
+//softShadows();
 
 extend({ OrbitControls });
 const CameraControls = () => {
@@ -50,8 +54,8 @@ function App() {
       <fog attach="fog" args={["white", 0, 60]} />
       <CameraControls position={[0, 0, 0]}/>
       <directionalLight castShadow shadowMap={true} shadowBias={-0.0001} shadow-mapSize-height={1024}
-  shadow-mapSize-width={1024} intensity={1}  position={[0, 7 ,20]}  />
-  <hemisphereLight color={"lightblue"} groundColor={"grey"} intensity={0.6} />
+  shadow-mapSize-width={1024} intensity={0.7}  position={[0, 7 ,20]}  />
+  <hemisphereLight color={"lightblue"} groundColor={"grey"} intensity={0.3} />
       <mesh castShadow receiveShadow rotation={[-3.14/2, 0, 0]} position={[0, 0, 0]} scale={[100,100,1]} >
         <planeBufferGeometry/>
         <meshStandardMaterial color="grey" />
@@ -66,7 +70,16 @@ function App() {
       <Store />
       </Suspense>
       
-     
+     <Sky 
+     sunPosition={[0, 1, 0]}
+     turbidity={2.1}
+     rayleigh={0.109}
+     mieCoefficient={0.006}
+     mieDirectionalG={0.941}
+     //inclination={0.4749}
+     //azimuth={0.2147}
+     exposure={0.18}
+     />
     </Canvas>
   );
 }
